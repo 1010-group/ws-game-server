2ba26f19076d537d50b97c822f5b612c2e138dc5
Bekzodmirzaaliyev <bekzodmirzaaliyev27@gmail.com>
2025-05-02 10:20:18 +0500

    ASD

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use("/api/v1/", userRouter);
 // Socket.IO setup
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["*"],
     methods: ["GET", "POST"],
   },
 });
